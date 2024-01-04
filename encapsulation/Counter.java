c2969de70d572ffbe955538316ba6d738ca13104
Dmitry <dmitry.guslov-gans@yandex.ru>
2024-01-04 17:35:07 +0300

    Написал программу, имитирующую работу реестра домашних животных.

---
+++ encapsulation/Counter.java
@@ -0,0 +1,21 @@
+package encapsulation;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+class Counter implements AutoCloseable {
+    private int count = 0;
+
+    public void add() {
+        this.count++;
+    }
+
+    @Override
+    public void close() {
+        // TODO реализовать логику проверки использования try-with-resources
+        // если не в блоке - throw new Exception()
+        // В данном примере реализована не будет
+    }
+}
---
+++ encapsulation/Main.java
@@ -0,0 +1,17 @@
+package encapsulation;
+
+import encapsulation.animals.types.*;
+import java.util.List;
+
+public class Main {
+    public static final String ANIMALS_TYPES_PACKAGE = "animals.types";
+
+    public static void main(String[] args) {
+        // TODO Перенести в меню
+        // Animals.Dog.add("Шарик");
+        // Horse horse = Animals.Horse.add(List.of("галоп"));
+        // horse.addCommand("test");
+        // Animals.list();
+        Menu.view();
+    }
+}
